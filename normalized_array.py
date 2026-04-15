86f6bcedaacbdf74d7d9cd9002bf86075dde87db
OrBarnetz <orba8@ac.sce.ac.il>
2026-04-16 01:21:52 +0300

    Update normalized_array.py

--- normalized_array.py
+++ normalized_array.py
@@ -1,12 +1,16 @@
 import numpy as np
 
+# a comment for rerunning grading
 def normalized_array(input_array):
-  data = np.array(input_array)
-  if np.all(data == data[0]):
-    return np.zeros(data.shape)
-  else:
-    new_array = (data - np.min(data)) / (np.max(data) - np.min(data))
-  return new_array
+    data = input_array.copy()
+    data = np.array(data)
+    
+    if np.min(data) == np.max(data):
+        return np.zeros(data.shape)
+    else:
+        data = (data - np.min(data)) / (np.max(data) - np.min(data))
+    
+    return data
 
 if __name__ == "__main__":
     # כאן הסטודנטים יכולים להריץ בדיקה עצמית מהירה
